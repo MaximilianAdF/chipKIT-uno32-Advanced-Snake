362b36877e09477d195c8c74ed3e1a8883f0e973
SexyFatBanana <150460527+SexyFatBanana@users.noreply.github.com>
2023-12-08 17:58:28 +0100

    Update gameover.c

--- gameover.c
+++ gameover.c
@@ -5,6 +5,8 @@
 int curr=0;
 int num=0;
 char Highscore_text[]=" New Highscore       V                AAA:___           ^       ";
+
+
 void gameover(int score,int oppownent){
     if(oppownent>0){
         if(score<oppownent){
@@ -18,11 +20,14 @@ void gameover(int score,int oppownent){
     }else{
     display_string(0,">   Game over                                        ");
 	display_update();}
+    char x=0;
+
+    while(x=0){
+        x=getbtns(0);
+    }
 
     //click ok
-
-    char current_scores[66]=read();
-    char new_scores[66]=new_highscore(current_scores);
+    char new_scores[66]=new_highscore(score);
     write(new_scores);
     // gå till main screen
 }
@@ -30,8 +35,23 @@ void gameover(int score,int oppownent){
 
 
 
-char write(){
+char write(array){
+    // Open the file in write mode
+    FILE *filePointer = fopen("highscore.txt", "w");
 
+    // Check if the file opened successfully
+    if (filePointer == NULL) {
+        printf("Unable to open the file.\n");
+        return 1; // Exit the program with an error code
+    }
+
+    // Write the char array to the file using fprintf
+    fprintf(filePointer, "%s", array);
+
+    // Close the file
+    fclose(filePointer);
+
+    return 0; 
 }
 
 char read() {
@@ -57,7 +77,7 @@ char read() {
 }
 
 
-char highscore_swap(char *highscores[],int score,int poss){
+char highscore_swap(char highscores[],int score,int poss){
     // Extract digits one by one
     char buffer[3];
     int i=0;
@@ -68,8 +88,9 @@ char highscore_swap(char *highscores[],int score,int poss){
         score /= 10;
     }
     for (int i=0; i<3;i++){
-        *highscores[9+i+poss*16]= buffer[i];
+        highscores[9+i+poss*16]= buffer[i];
     }
+    return highscores;
 }
 
 
@@ -81,8 +102,10 @@ char new_highscore(score){
             sum +=atoi(highscores[9+j+i*16])*pow(10,2-j);
         }
         if (score>sum){
-            highscore_swap(&highscores,score,i);
+
+            char highscore_new[]=highscore_swap(highscores,score,i);
             score = sum;
+            return highscore_new;
         }
     }
     return highscores;
